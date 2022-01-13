a4395edf1cfcddde16b644da708a5c7c61a2a5bd
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2022-01-13 12:41:59 +0100

    t add ignored golden master test capturing program output
    Currently ignored because it depends on system time

--- expensereport-kotlin/src/test/kotlin/org/nelkinda/training/HelloTest.kt
+++ expensereport-kotlin/src/test/kotlin/org/nelkinda/training/HelloTest.kt
@@ -1,6 +1,7 @@
 package org.nelkinda.training
 
 import com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOut
+import org.approvaltests.Approvals
 import org.junit.Ignore
 import org.junit.Test
 import kotlin.test.assertEquals
@@ -33,6 +34,18 @@ class HelloTest {
         }
 
         // assert first line in report is correct format
+
+    }
+
+    @Test
+    @Ignore("Depends on current time")
+    fun `golden master test of program output`() {
+        val noExpenses = emptyList<Expense>()
+
+        val actual = tapSystemOut {
+            ExpenseReport().printReport(noExpenses)
+        }
+        Approvals.verify(actual)
     }
 
 }
