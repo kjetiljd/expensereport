9c5dbdcf5f110177dd8854a4f7b51920e9507561
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2022-01-13 14:22:48 +0100

    e add clarification that git-fetch-changes does not push

--- expensereport-kotlin/git-fetch-changes.sh
+++ expensereport-kotlin/git-fetch-changes.sh
@@ -6,7 +6,7 @@ do
   changes=$(git log HEAD..origin/trunk);
   if [ -z "$changes" ]
   then
-    echo "No changes detected";
+    echo "No changes detected. Remember to push your own changes!";
     sleep 30;
   else
     git show HEAD..origin/trunk && git pull --rebase;
